0be500febf038b625fbe1dea0725ba5140861199
Administrator <Administrator@MANMAN-WEI>
2025-02-20 17:52:43 +0000

    添加项目文件。

---
+++ Projectwebsite/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Video()
+        {
+            return View();
+        }
+
+        public IActionResult Members()
+        {
+            return View();
+        }
+
+        public IActionResult Background()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projectwebsite/Views/Home/Background.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projectwebsite.Views.Home
+{
+    public class BackgroundModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
